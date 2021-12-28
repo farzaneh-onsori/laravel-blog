29066a1fbdd7c07be9b0f95dc1e788cea365595d
Farzaneh Onsori <farzaneh.onsori@yahoo.com>
2021-12-28 16:47:28 +0100

    Make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+   
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, ut. Cumque possimus sit, voluptatem expedita omnis qui fugit dolore? Incidunt at minus aut sit amet consequatur iste, et harum quaerat?</p>
+    </article>
+    
+    <a href="/">Go Back</a>
+</body>
+</html>
