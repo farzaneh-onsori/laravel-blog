8a44da30b0377f20aaeb2246d185475b93230d3c
Farzaneh Onsori <farzaneh.onsori@yahoo.com>
2021-12-28 16:47:28 +0100

    Store Blog Posts as HTML Files

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css">
+   
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, ut. Cumque possimus sit, voluptatem expedita omnis qui fugit dolore? Incidunt at minus aut sit amet consequatur iste, et harum quaerat?</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, ut. Cumque possimus sit, voluptatem expedita omnis qui fugit dolore? Incidunt at minus aut sit amet consequatur iste, et harum quaerat?</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, ut. Cumque possimus sit, voluptatem expedita omnis qui fugit dolore? Incidunt at minus aut sit amet consequatur iste, et harum quaerat?</p>
+    </article>
+    
+</body>
+</html>
